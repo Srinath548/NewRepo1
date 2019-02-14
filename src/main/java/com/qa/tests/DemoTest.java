4d465e15342ece50f076b62ab60181c0597c7b4e
Shivva <ssrinath@corp.untd.com>
2019-02-14 15:11:14 +0530

    Pushing initial changes.

---
+++ src/main/java/com/qa/tests/DemoTest.java
@@ -0,0 +1,52 @@
+package com.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+
+public class DemoTest {
+	
+	@Test
+	public void sum()
+	{		
+		System.out.println("Addition");
+		int a=50;
+		int b=30;
+		int c=20;
+		int d=40;
+		int e=60;
+		int f=100;
+		Assert.assertEquals(300, a+b+c+d+e+f);		
+	}
+	
+	@Test
+	public void substraction()
+	{		
+		System.out.println("Substraction");
+		int a=50;
+		int b=70;
+		Assert.assertEquals(20, b-a);		
+	}
+	
+	@Test
+	public void div()
+	{	
+		System.out.println("Division");
+		int a=80;
+		int b=20;
+		Assert.assertEquals(4, a/b);		
+	}
+	
+	@Test
+	public void multi()
+	{		
+		System.out.println("Multiplication");
+		int a=5;
+		int b=10;
+		Assert.assertEquals(50, a*b);		
+	}
+	
+	
+
+}
---
+++ src/test/java/com/qa/pages/Homepage.java
@@ -0,0 +1,12 @@
+package com.qa.pages;
+
+public class Homepage {
+
+	public void homePage()
+	{
+		System.out.println("Home page");
+	}
+	
+	
+	
+}
