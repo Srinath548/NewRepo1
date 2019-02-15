9f75839b171cf8353c613820b47e59b2e0a78fff
Shivva <ssrinath@corp.untd.com>
2019-02-15 13:47:27 +0530

    Modified the pwd page file.

--- src/test/java/com/qa/pages/UpdatePwdPage.java
+++ src/test/java/com/qa/pages/UpdatePwdPage.java
@@ -6,8 +6,7 @@ public class UpdatePwdPage {
 		public void updatePwdPage()
 		{
 			System.out.println("Update Password page.");
+			System.out.println("Please dont use the most recent three passwords.");
 		}
-
-
 
 }
